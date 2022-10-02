83f4f07064202d41af4e7f167f0dfd03beacdd38
Bibash <bibash.kafle@gmai.com>
2022-10-02 00:58:39 -0400

    added electron service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private _electronService: ElectronService) { }
   title = 'electron-app';
 }
